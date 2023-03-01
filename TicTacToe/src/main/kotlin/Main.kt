a7dc7c37944f502ff0f35cef8da66c5312f0c5e2
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-01 17:01:36 -0500

    Check winner

--- TicTacToe/src/main/kotlin/Main.kt
+++ TicTacToe/src/main/kotlin/Main.kt
@@ -26,12 +26,24 @@ fun main() {
             val positions = input.split(",")
             x = positions[0].trim().toInt()
             y = positions[1].trim().toInt()
+            var skipRound = false
 
             if (board[x - 1][y - 1] != "") {
                 println("That position is already taken, please try again")
+                skipRound = true
             } else {
                 board[x - 1][y - 1] = "X"
                 printBoard()
+            }
+
+            if (!skipRound) {
+                val playerWon = checkWinner(true)
+                if (playerWon) {
+                    println("\uD83C\uDF7E \uD83C\uDF8A \uD83C\uDF89 \uD83C\uDF7E \uD83C\uDF8A \uD83C\uDF89 \uD83C\uDF7E \uD83C\uDF8A \uD83C\uDF89")
+                    println("You won! Congratulations")
+
+                    continueGame = false
+                }
             }
 
         } catch (e: Exception) {
@@ -54,4 +66,34 @@ fun printBoard() {
         println("|")
         println("-------------")
     }
+}
+
+fun checkWinner(player: Boolean): Boolean {
+    var won = false
+    val checkSymbol = if (player) "X" else "O"
+
+    for (i in 0..2) {
+        // Horizontal wins
+        if (board[i][0] == checkSymbol && board[i][1] == checkSymbol && board[0][2] == checkSymbol) {
+            won = true
+            break
+        }
+
+        // Vertical wins
+        if (board[0][i] == checkSymbol && board[1][i] == checkSymbol && board[2][i] == checkSymbol) {
+            won = true
+            break
+        }
+    }
+
+    // Diagonal wins
+    if (board[0][0] == checkSymbol && board[1][1] == checkSymbol && board[2][2] == checkSymbol) {
+        won = true
+    }
+
+    if (board[2][0] == checkSymbol && board[1][1] == checkSymbol && board[0][2] == checkSymbol) {
+        won = true
+    }
+
+    return won
 }
