ca0841d4c970bb37e7371f60bf9c90ef8e121353
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-02 13:38:52 -0500

    Inheritance 3

---
+++ OOP/src/main/kotlin/inheritance/Bird.kt
@@ -0,0 +1,27 @@
+package inheritance
+
+fun main() {
+
+    val myParakeet = Parakeet()
+    myParakeet.fly()
+}
+
+open class Bird {
+
+    open val color = "grey"
+    open val speed = 50
+
+    open fun fly() {
+        println("A $color bird can fly up to $speed km/h")
+    }
+}
+
+class Parakeet : Bird() {
+    override val color = "green"
+    override val speed = 65
+
+    override fun fly() {
+        super.fly()
+        println("A ${super.color} bird can fly up to ${super.speed} km/h")
+    }
+}
