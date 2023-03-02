ca52b69c2c0b48fac9c85623d05d0e86e2fd8db6
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-01 19:40:28 -0500

    Object constructor

--- OOP/src/main/kotlin/Main.kt
+++ OOP/src/main/kotlin/Main.kt
@@ -1,23 +1,32 @@
 fun main() {
 
-//    val myCar = Car()
-//    val yourCar = Car()
-//
-//    myCar.model = "BMW"
-//    yourCar.model = "Toyota"
-//
-//    myCar.topSeed = 140
-//    myCar.start()
-//    myCar.drive(50)
-//
-//    yourCar.start()
-//    yourCar.drive(60)
+    val myCar = Car()
+    val yourCar = Car()
+
+    myCar.model = "BMW"
+    yourCar.model = "Toyota"
+
+    myCar.topSeed = 140
+    myCar.start()
+    myCar.drive(50)
+
+    yourCar.start()
+    yourCar.drive(60)
 
     val math = Math()
     println(math.add(1, 2))
     println(math.sub(1, 2))
     println(math.mul(1, 2))
     println(math.div(1, 2.0))
+
+    DatabaseAccess.connected = true
+    if (DatabaseAccess.connected) {
+        DatabaseAccess.disconnect()
+    } else {
+        DatabaseAccess.connect()
+    }
+
+    println("Database is connected: ${DatabaseAccess.connected}")
 }
 
 class Car {
@@ -41,4 +50,18 @@ class Math {
     fun mul(a: Int, b: Int) = a * b
 
     fun div(a: Int, b: Double): Double = a / b
+}
+
+object DatabaseAccess {
+    var connected = false
+
+    fun connect() {
+        connected = true
+        println("Database connected")
+    }
+
+    fun disconnect() {
+        connected = false
+        println("Databases disconnected")
+    }
 }
