b88068d2b22b22728a1f9c77590cdf14b511a0c1
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-01 19:20:05 -0500

    Math class

--- OOP/src/main/kotlin/Main.kt
+++ OOP/src/main/kotlin/Main.kt
@@ -1,17 +1,23 @@
 fun main() {
 
-    val myCar = Car()
-    val yourCar = Car()
+//    val myCar = Car()
+//    val yourCar = Car()
+//
+//    myCar.model = "BMW"
+//    yourCar.model = "Toyota"
+//
+//    myCar.topSeed = 140
+//    myCar.start()
+//    myCar.drive(50)
+//
+//    yourCar.start()
+//    yourCar.drive(60)
 
-    myCar.model = "BMW"
-    yourCar.model = "Toyota"
-
-    myCar.topSeed = 140
-    myCar.start()
-    myCar.drive(50)
-
-    yourCar.start()
-    yourCar.drive(60)
+    val math = Math()
+    println(math.add(1, 2))
+    println(math.sub(1, 2))
+    println(math.mul(1, 2))
+    println(math.div(1, 2.0))
 }
 
 class Car {
@@ -25,4 +31,14 @@ class Car {
     fun drive(speed: Int) {
         println("Driving at a speed to $speed")
     }
+}
+
+class Math {
+    fun add(a: Int, b: Int) = a + b
+
+    fun sub(a: Int, b: Int) = a - b
+
+    fun mul(a: Int, b: Int) = a * b
+
+    fun div(a: Int, b: Double): Double = a / b
 }
