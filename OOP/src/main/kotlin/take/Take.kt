e466ae495a5238b08ac2da789d88acab6e75f8fa
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-06 17:48:11 -0500

    Check functions

--- OOP/src/main/kotlin/take/Take.kt
+++ OOP/src/main/kotlin/take/Take.kt
@@ -12,4 +12,28 @@ fun main() {
   val oddOrNull = number.takeUnless { it % 2 == 0 }
 
   println(oddOrNull)
+
+  //////////////////////////////////////////////////////////
+
+  val numbers = listOf(3, 56, 45, 67, 89, 0, 2, 6, 78)
+  println(numbers)
+
+  val newNumbers = arrayListOf<Int>()
+  for (n in numbers) {
+    n.takeIf { it % 2 == 0 }
+      ?.let { newNumbers.add(it) }
+  }
+
+  println(newNumbers)
+
+  val clients = listOf("Bob", "Alex", "Carlo", "Dan")
+  println(clients)
+
+  val newClients = arrayListOf<String>()
+  for (client in clients) {
+    client.takeUnless { it[0] == 'A' }
+      ?.let { newClients.add(it) }
+  }
+
+  println(newClients)
 }
