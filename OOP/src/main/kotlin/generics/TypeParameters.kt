5631c22db3cab57f63a33c6a03dbdf49c651b324
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-07 14:56:50 -0500

    type constraints

---
+++ OOP/src/main/kotlin/generics/TypeParameters.kt
@@ -0,0 +1,35 @@
+package generics
+
+fun main() {
+  val chef = Chef<Apple>()
+  chef.cook(Apple())
+
+  val chef2 = Chef<Banana>()
+  chef2.cook(Banana())
+}
+
+abstract class Fruit {
+
+  abstract fun peel()
+}
+
+class Apple : Fruit() {
+
+  override fun peel() {
+    println("Peeling the apple")
+  }
+}
+
+class Banana : Fruit() {
+
+  override fun peel() {
+    println("Peeling the banana")
+  }
+}
+
+class Chef<T : Fruit> {
+
+  fun cook(item: T) {
+    item.peel()
+  }
+}
