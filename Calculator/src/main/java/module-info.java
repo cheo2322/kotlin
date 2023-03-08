c0b4f3dd26b4f56c208a1ab5510000f3f4713a21
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-08 10:50:42 -0500

    Calculator: GUI

---
+++ Calculator/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.calculator {
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires kotlin.stdlib;
+
+  requires org.controlsfx.controls;
+  requires org.kordamp.bootstrapfx.core;
+
+  opens com.example.calculator to javafx.fxml;
+  exports com.example.calculator;
+}
