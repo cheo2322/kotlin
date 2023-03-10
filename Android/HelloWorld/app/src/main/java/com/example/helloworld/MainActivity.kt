83bf4b9ceb5daa97179fe9b63e5da6714835ff4a
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-10 15:30:24 -0500

    Android: App functionality

--- Android/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ Android/HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(v: View) {
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_LONG).show()
     }
 }
