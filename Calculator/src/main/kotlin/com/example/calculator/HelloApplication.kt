91f99c8a98cfdbbf6d505dea502f37d18caa74b3
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-10 14:15:58 -0500

    Calculator: Functionality and styles

--- Calculator/src/main/kotlin/com/example/calculator/HelloApplication.kt
+++ Calculator/src/main/kotlin/com/example/calculator/HelloApplication.kt
@@ -8,7 +8,7 @@ import javafx.stage.Stage
 class HelloApplication : Application() {
   override fun start(stage: Stage) {
     val fxmlLoader = FXMLLoader(HelloApplication::class.java.getResource("hello-view.fxml"))
-    val scene = Scene(fxmlLoader.load(), 400.0, 600.0)
+    val scene = Scene(fxmlLoader.load())
     stage.title = "Calculator"
     stage.scene = scene
     stage.show()
--- Calculator/src/main/kotlin/com/example/calculator/HelloController.kt
+++ Calculator/src/main/kotlin/com/example/calculator/HelloController.kt
@@ -1,14 +1,55 @@
 package com.example.calculator
 
+import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.control.Label
 
 class HelloController {
-  @FXML
-  private lateinit var welcomeText: Label
 
   @FXML
-  private fun onHelloButtonClick() {
-    welcomeText.text = "Welcome to JavaFX Application!"
+  private lateinit var display: Label
+
+  private var state: Operator = Operator.add(0)
+
+  private fun onAction(fn: Operator) {
+    state = fn
+    display.text = ""
+  }
+
+  private val displayValue: Long
+    get() = when (display.text) {
+      "" -> 0
+      else -> display.text.toLong()
+    }
+
+  private fun operator(x: String) {
+    if (Regex("[0-9]").matches(x)) {
+      display.text += x
+    } else {
+      when (x) {
+        "+" -> onAction(Operator.add(displayValue))
+        "-" -> onAction(Operator.sub(displayValue))
+        "/" -> onAction(Operator.div(displayValue))
+        "X" -> onAction(Operator.mul(displayValue))
+        "%" -> {
+          onAction(Operator.add(displayValue / 100))
+          operator("=")
+        }
+
+        "C" -> onAction(Operator.add(0))
+        "+/-" -> {
+          onAction(Operator.add(-1 * displayValue))
+          operator("=")
+        }
+
+        "=" -> display.text = state.calculate(displayValue).toString()
+      }
+    }
+  }
+
+  @FXML
+  fun pressButton(actionEvent: ActionEvent) {
+    operator(actionEvent.source.toString().split('\'')[1])
+    println(actionEvent.source.toString())
   }
 }
