da66bdbafc442c0582e78c6857b12ccdd54d897e
Sergio Hidalgo <barcecheo2319@gmail.com>
2023-03-20 09:04:18 -0500

    ImageProcessingCoroutines: adding logic

--- Android/ImageProcessingCoroutines/app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
+++ Android/ImageProcessingCoroutines/app/src/main/java/com/devtides/imageprocessingcoroutines/MainActivity.kt
@@ -1,14 +1,50 @@
 package com.devtides.imageprocessingcoroutines
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private val IMAGE_URL = "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val IMAGE_URL =
+        "https://raw.githubusercontent.com/DevTides/JetpackDogsApp/master/app/src/main/res/drawable/dog.png"
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        coroutineScope.launch {
+            val originalDeferred = coroutineScope.async(Dispatchers.IO) {
+                getOriginalBitmap()
+            }
+
+            val originalBitMap =originalDeferred.await()
+
+            loadImage(originalBitMap)
+        }
+    }
+
+    private fun getOriginalBitmap() =
+        URL(IMAGE_URL).openStream().use {
+            BitmapFactory.decodeStream(it)
+        }
+
+    private fun loadImage(bmp: Bitmap) {
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        progressBar.visibility = View.GONE
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        imageView.setImageBitmap(bmp)
+        imageView.visibility = View.VISIBLE
     }
 }
